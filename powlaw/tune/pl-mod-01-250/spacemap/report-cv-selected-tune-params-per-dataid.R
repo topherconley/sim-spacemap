a66ee99ffd65f5700238bdb2b286bc7ee6b5015f
cconley <christopher.conley@gmail.com>
2016-09-07 21:44:02 -0700

    Power-law boot.vote additions.

---
+++ powlaw/tune/pl-mod-01-250/spacemap/report-cv-selected-tune-params-per-dataid.R
@@ -0,0 +1,21 @@
+
+setwd("~/scratch-data/sim-spacemap/powlaw/pl-mod-01/results/n250/spacemap")
+
+top <- new.env()
+load(envir = top, file = "spacemap_top_cv_voting.rda")
+best_path <- sapply(top$best_holdout_files, function(x) dirname(x[1]))
+best_path <- gsub(pattern = "/home/cconley", "~", best_path)
+best_cv_object_file <- file.path(best_path, paste0("cv_object_dataid_", 1:100, ".rds"))
+
+#get data id
+get_tune <- function(f) { 
+  tmp <- readRDS(f) 
+  res <- tmp$tune
+  ddid <- regmatches(x = f, m = regexpr(pattern = "d[0-9]{3}", text = f))
+  res$dataid <- as.integer(sub("d", "", ddid))
+  res
+}
+
+best_tune <- do.call(rbind,lapply(best_cv_object_file, get_tune))
+saveRDS(best_tune, file = "cv_selected_tune_params_per_dataid.rds")
+
